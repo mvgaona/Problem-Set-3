3b7b1feb0ff855d87da8c9aaf61ebbe87cd169a8
ambelenoh <a.beleno@uniandes.edu.co>
2022-07-20 20:26:21 -0500

    Incorporación  de código para extracción de datos
    Actualización del código para la extracción de datos de baños y área,
     y creación de datos georreferenciados para distancia mínima a bares y a transporte público

--- Script/Archivo oficial Problem Set 3.R
+++ Script/Archivo oficial Problem Set 3.R
@@ -32,6 +32,7 @@ p_load(caret,
        sf,
        leaflet,
        tmaptools,
+       nngeo,
        osmdata)
 rm(list = ls()) #Limpia las variables que existan al momento de correr el código
 ###Base de datos Problem set 2
@@ -145,6 +146,201 @@ table(DTRAIN$l3)
 table(DTEST$l3)
 table(is.na(DTRAIN$surface_total))
 table(is.na(DTRAIN$surface_covered))
+
+#Vamos a obtener los datos de baños y área 
+
+#Para área
+pat_1 <- "[:space:]+[:digit:]+[:punct:]+[:digit:]+[:space:]+m2" ## patrón para área
+pat_2 <- "[:space:]+[:digit:]+[:punct:]+[:digit:]+m2"
+pat_3<-"[:space:]+[:digit:]+[:space:]+metros"
+pat_4<-"[:space:]+[:digit:]+metros"
+pat_5 <- "[:space:]+[:digit:]+[:punct:]+[:digit:]+[:space:]+mts"
+pat_6 <- "[:space:]+[:digit:]+mts"
+pat_7 <- "[:space:]+[:digit:]+[:space:]+mts"
+pat_8<-"[:space:]+[:digit:]+m^2"
+pat_9 <- "[:space:]+[:digit:]+[:punct:]+[:digit:]+[:space:]+m^2"
+pat_10 <- "[:space:]+[:digit:]+[:punct:]+[:digit:]+[:space:]+mtrs"
+pat_11 <- "[:space:]+[:digit:]+mtrs"
+pat_12 <- "[:space:]+[:digit:]+[:space:]+mtrs"
+pat_13 <- "[:space:]+[:digit:]+[:space:]+[:punct:]+[:digit:]+[:space:]+mts"
+pat_14 <- "[:space:]+[:digit:]+[:punct:]+[:digit:]+[:space:]+mt"
+pat_15 <- "[:space:]+[:digit:]+mt"
+pat_16 <- "[:space:]+[:digit:]+[:space:]+mt"
+pat_17 <- "[:space:]+[:digit:]+[:space:]+[:punct:]+[:digit:]+[:space:]+mts2"
+pat_18 <- "[:space:]+[:digit:]+[:punct:]+[:digit:]+[:space:]+mts2"
+pat_19 <- "[:space:]+[:digit:]+mts2"
+pat_20 <- "[:space:]+[:digit:]+[:space:]+mts2"
+pat_21 <- "[:space:]+[:digit:]+[:space:]+[:punct:]+[:digit:]+mts2"
+pat_22 <- "[:space:]+[:digit:]+[:space:]+[:punct:]+[:digit:]+[:space:]+metros cuadrados"
+pat_23 <- "[:space:]+[:digit:]+[:punct:]+[:digit:]+[:space:]+metros cuadrados"
+pat_24 <- "[:space:]+[:digit:]+metros cuadrados"
+pat_25 <- "[:space:]+[:digit:]+[:space:]+metros cuadrados"
+pat_26 <- "[:space:]+[:digit:]+[:space:]+[:punct:]+[:digit:]+[:space:]+metros cuadrados"
+
+
+DTRAIN <- DTRAIN  %>% 
+    mutate(area_total= str_extract(string=DTRAIN$description , pattern= paste0(pat_1,"|",pat_2,"|", pat_3,"|", pat_4,"|", pat_5,"|", pat_6,"|", pat_7,"|", pat_8,"|", pat_9,"|", pat_10,"|", pat_11,"|", pat_12,"|", pat_13,"|", pat_14,"|", pat_15,"|", pat_16,"|", pat_17,"|", pat_18,"|", pat_19,"|", pat_20,"|", pat_21,"|", pat_22,"|", pat_23,"|", pat_24,"|", pat_25,"|", pat_26) ))
+
+DTRAIN$area_total<-str_replace_all(string = DTRAIN$area_total, pattern = "," , replacement = ".")
+DTRAIN$area_total<-str_replace_all(string = DTRAIN$area_total, pattern = "m2" , replacement = "")
+DTRAIN$area_total<-str_replace_all(string = DTRAIN$area_total, pattern = "mts" , replacement = "")
+DTRAIN$area_total<-str_replace_all(string = DTRAIN$area_total, pattern = "mtrs" , replacement = "")
+DTRAIN$area_total<-str_replace_all(string = DTRAIN$area_total, pattern = "metros" , replacement = "")
+DTRAIN$area_total<-str_replace_all(string = DTRAIN$area_total, pattern = "m^2" , replacement = "")
+DTRAIN$area_total<-str_replace_all(string = DTRAIN$area_total, pattern = "mt" , replacement = "")
+DTRAIN$area_total<-str_replace_all(string = DTRAIN$area_total, pattern = "metros cuadrados" , replacement = "")
+DTRAIN$area_total<-str_replace_all(string = DTRAIN$area_total, pattern = "cuadrados" , replacement = "")
+DTRAIN$area_total<-str_replace_all(string = DTRAIN$area_total, pattern = "\n" , replacement = "")
+
+DTRAIN$area_total<-as.numeric(DTRAIN$area_total)
+
+new_area_aux<-cbind(DTRAIN$surface_total, DTRAIN$surface_covered)
+new_area_aux_2<-apply(new_area_aux, 1 , max)
+new_area_aux_2<-data.frame(new_area_aux_2)
+
+mnzBog<-readRDS("../Elementos_Guardados/Bogota.rds") #Datos de manzanas Bogotá
+mnzAnt<-readRDS("../Elementos_Guardados/Antioquia.rds") #Datos de manzanas Antioquia
+
+mnzBogota<-subset(mnzBog, select=c("MANZ_CCNCT", "geometry"))
+mnzAntioquia<-subset(mnzAnt, select=c("MANZ_CCNCT", "geometry"))
+
+DTRAIN_Bog<- DTRAIN_sf[DTRAIN_sf$l3=="Bogotá D.C",]
+DTRAIN_Med<- DTRAIN_sf[DTRAIN_sf$l3=="Medellín",]
+
+sf::sf_use_s2(FALSE)
+
+mnzBogota<-st_transform(mnzBogota, 4326)
+
+DTRAIN_Bog<-DTRAIN_Bog %>% st_as_sf(coords = c("lon", "lat"), crs = 4326)
+
+st_crs(mnzBogota) == st_crs(DTRAIN_Bog)
+
+Casa_mnz_aux<- st_join(DTRAIN_Bog,mnzBogota,join = nngeo::st_nn, maxdist = 50, k = 1, progress = FALSE)
+
+
+Casa_mnz_aux2 = st_join(DTRAIN_Med,mnzAntioquia, join = nngeo::st_nn, maxdist = 50, k = 1, progress = FALSE)
+
+leaflet() %>% addTiles() %>% addPolygons(data=mnzBogota , color="red") %>% addCircles(data=Casa_mnz_aux)
+
+
+## Promedio de la manzana
+Casa_mnz_aux = Casa_mnz_aux %>%
+  group_by(MANZ_CCNCT) %>%
+  mutate(nueva_area=median(surface_total,na.rm=T))
+
+table(is.na(Casa_mnz_aux$surface_total))
+
+table(is.na(Casa_mnz_aux$surface_total),
+      is.na(Casa_mnz_aux$nueva_area)) # a
+
+## k-vecinos espaciales
+test = is.na(Casa_mnz_aux$surface_total)
+no_test = is.na(Casa_mnz_aux$surface_total)==F
+
+
+k1 = knn(train=Casa_mnz_aux[no_test,c("geometry","surface_total")], ## base de entrenamiento
+         test=Casa_mnz_aux[test,c("geometry","surface_total")],   ## base de testeo
+         cl=Casa_mnz_aux$surface_total[test], ## outcome
+         k=1)  
+
+
+table(is.na(Casa_mnz_aux$MANZ_CCNCT))
+
+DTRAIN[12810,]
+
+area_tot<- apply(new_area_aux, 1 , max)
+
+
+area_tot<-data.frame(area_tot)
+summary(area_tot)
+
+DTRAIN<-cbind(DTRAIN, area_tot )
+
+
+table(house$new_surface)
+
+DTRAIN[106000,]
+
+
+#Para obtener los datos de los baños
+
+pat_1b <- "[:space:]+[:digit:]+[:space:]+baño" ## patrón para baños
+pat_2b <- "[:space:]+[:digit:]+[:space:]+baños"
+pat_3b<-"[:space:]+[:digit:]+baño"
+pat_4b<-"[:space:]+[:digit:]+baños"
+pat_5b <- "baño+[:space:]+[:digit:]"
+pat_6b <- "baños+[:space:]+[:digit:]"
+pat_7b <- "[:space:]+[:digit:]+[:space:]+banio"
+pat_8b<-"[:space:]+[:digit:]+[:space:]+banios"
+pat_9b <- "[:space:]+[:digit:]+banios"
+pat_10b <- "banio+[:space:]+[:digit:]"
+pat_11b <- "banios+[:space:]+[:digit:]"
+pat_12b <- "[:space:]+[:digit:]+[:space:]+bano"
+pat_13b <- "[:space:]+[:digit:]+[:space:]+banos"
+pat_14b <- "[:space:]+[:digit:]+bano"
+pat_15b <- "[:space:]+[:digit:]+banos"
+pat_16b <- "bano+[:space:]+[:digit:]"
+pat_17b <- "banos+[:space:]+[:digit:]"
+pat_18b <- "[:space:]+[:digit:]+[:space:]+vano"
+pat_19b <- "[:space:]+[:digit:]+[:space:]+vanos"
+pat_20b <- "[:space:]+[:digit:]+vano"
+pat_21b <- "[:space:]+[:digit:]+vanos"
+pat_22b <- "vano+[:space:]+[:digit:]"
+pat_23b <- "vanos+[:space:]+[:digit:]"
+pat_24b <- "[:space:]+[:digit:]+[:space:]+vanio"
+pat_25b <- "[:space:]+[:digit:]+[:space:]+vanios"
+pat_26b <- "[:space:]+[:digit:]+vanio"
+pat_27b<-"[:space:]+[:digit:]+vanios"
+pat_28b <- "vanio+[:space:]+[:digit:]"
+pat_29b <- "vanios+[:space:]+[:digit:]"
+pat_30b <- "[:space:]+[:digit:]+[:space:]+vaño"
+pat_31b <- "[:space:]+[:digit:]+[:space:]+vaños"
+pat_32b <- "[:space:]+[:digit:]+vaño"
+pat_33b<-"[:space:]+[:digit:]+vaños"
+pat_34b <- "vaño+[:space:]+[:digit:]"
+pat_35b <- "vaños+[:space:]+[:digit:]"
+pat_36b <- "[:space:]+[:digit:]+[:space:]+bañio"
+pat_37b <- "[:space:]+[:digit:]+[:space:]+bañios"
+pat_38b <- "[:space:]+[:digit:]+bañio"
+pat_39b<-"[:space:]+[:digit:]+bañios"
+pat_40b <- "bañio+[:space:]+[:digit:]"
+pat_41b <- "bañios+[:space:]+[:digit:]"
+pat_42b <- "[:space:]+[:digit:]+[:space:]+bao"
+pat_43b <- "[:space:]+[:digit:]+[:space:]+baos"
+pat_44b <- "[:space:]+[:digit:]+bao"
+pat_45b<-"[:space:]+[:digit:]+baos"
+pat_46b <- "bao+[:space:]+[:digit:]"
+pat_47b <- "baos+[:space:]+[:digit:]"
+
+
+DTRAIN <- DTRAIN  %>% 
+  mutate(banio_tot= str_extract(string=DTRAIN$description , pattern= paste0(pat_1b,"|",pat_2b,"|", pat_3b,"|", pat_4b,"|", pat_5b,"|", pat_6b,"|", pat_7b,"|", pat_8b,"|", pat_9b,"|", pat_10b,"|", pat_11b,"|", pat_12b,"|", pat_13b,"|", pat_14b,"|", pat_15b,"|", pat_16b,"|", pat_17b,"|", pat_18b,"|", pat_19b,"|", pat_20b,"|", pat_21b,"|", pat_22b,"|", pat_23b,"|", pat_24b,"|", pat_25b,"|", pat_26b,"|", pat_27b,"|", pat_28b,"|", pat_29b,"|", pat_30b,"|", pat_31b,"|", pat_32b,"|", pat_33b,"|", pat_34b,"|", pat_35b,"|", pat_36b,"|", pat_37b,"|", pat_38b,"|", pat_39b,"|", pat_40b,"|", pat_41b,"|", pat_42b,"|", pat_43b,"|", pat_44b,"|", pat_45b,"|", pat_46b,"|", pat_47b) ))
+
+DTRAIN$banio_tot<-str_replace_all(string = DTRAIN$banio_tot, pattern = "," , replacement = ".")
+DTRAIN$banio_tot<-str_replace_all(string = DTRAIN$banio_tot, pattern = "baño" , replacement = "")
+DTRAIN$banio_tot<-str_replace_all(string = DTRAIN$banio_tot, pattern = "baños" , replacement = "")
+DTRAIN$banio_tot<-str_replace_all(string = DTRAIN$banio_tot, pattern = "banio" , replacement = "")
+DTRAIN$banio_tot<-str_replace_all(string = DTRAIN$banio_tot, pattern = "banios" , replacement = "")
+DTRAIN$banio_tot<-str_replace_all(string = DTRAIN$banio_tot, pattern = "bano" , replacement = "")
+DTRAIN$banio_tot<-str_replace_all(string = DTRAIN$banio_tot, pattern = "banos" , replacement = "")
+DTRAIN$banio_tot<-str_replace_all(string = DTRAIN$banio_tot, pattern = "vano" , replacement = "")
+DTRAIN$banio_tot<-str_replace_all(string = DTRAIN$banio_tot, pattern = "vanos" , replacement = "")
+DTRAIN$banio_tot<-str_replace_all(string = DTRAIN$banio_tot, pattern = "vanio" , replacement = "")
+DTRAIN$banio_tot<-str_replace_all(string = DTRAIN$banio_tot, pattern = "vanios" , replacement = "")
+DTRAIN$banio_tot<-str_replace_all(string = DTRAIN$banio_tot, pattern = "vaño" , replacement = "")
+DTRAIN$banio_tot<-str_replace_all(string = DTRAIN$banio_tot, pattern = "vaños" , replacement = "")
+DTRAIN$banio_tot<-str_replace_all(string = DTRAIN$banio_tot, pattern = "bañio" , replacement = "")
+DTRAIN$banio_tot<-str_replace_all(string = DTRAIN$banio_tot, pattern = "bañios" , replacement = "")
+DTRAIN$banio_tot<-str_replace_all(string = DTRAIN$banio_tot, pattern = "bao" , replacement = "")
+DTRAIN$banio_tot<-str_replace_all(string = DTRAIN$banio_tot, pattern = "baos" , replacement = "")
+DTRAINbanio_tot<-str_replace_all(string = DTRAIN$banio_tot, pattern = "\n" , replacement = "")
+
+
+
+
+##Para obtener datos de Open Street Map
+#===================================================================================
+
 DTRAIN[46,]
 DTRAIN_sf <- DTRAIN %>% st_as_sf(coords = c("lon", "lat"), crs = 4326)
 class(DTRAIN_sf) 
@@ -208,8 +404,7 @@ leaflet() %>% addTiles() %>% addCircleMarkers(data=Transporte_publicoBog, col="b
 p_load(rgdal)
 
 ##Extracción datos de manzanas
-mnzBogota<-readRDS("../Elementos_Guardados/Bogota.rds") #Datos de manzanas Bogotá
-mnzAntioquia<-readRDS("../Elementos_Guardados/Antioquia.rds") #Datos de manzanas Antioquia
+
 
 #Apartamentos
 CHAPINERO = getbb(place_name = "Chapinero Bogotá", 
@@ -236,19 +431,94 @@ leaflet() %>% addTiles() %>%
   addCircles(data=barbog , col="black" , weight=2)
 st_crs(mnzBogota) == st_crs(DTRAIN_sf)
 st_crs(mnzAntioquia) == st_crs(DTRAIN_sf)
-Uniondatbog = st_join(x=DTRAIN_sf , y=mnzBogota)
-Uniondatbog$dist_bar = st_distance(x=Uniondatbog , y=barbog)
-housing_p
+#Uniondatbog = st_join(x=DTRAIN_sf , y=mnzBogota)
+
+#Se calculará distancia a bares
+#Con respecto a bares Bogotá
+dist_bar_bog <- st_distance(x=DTRAIN_sf , y=barbog)
+min_dist_bog <- apply(dist_bar_bog , 1 , min)
+min_dist_bog<-data.frame(min_dist_bog)
+
+#Con respecto a bares Antioquia
+dist_bar_med <- st_distance(x=DTRAIN_sf , y=barant)
+min_dist_bar_med <- apply(dist_bar_med, 1 , min)
+min_dist_bar_med<-data.frame(min_dist_bar_med)
+
+#Se selecciona la menor distancia, esto porque se compararon distancias de apartamentos en Bogotá con ubicaciones en Antioquia y viceversa
+min_dist_bar<-cbind(min_dist_bog,min_dist_bar_med)
+min_dist_bar_<- apply(min_dist_bar, 1 , min)
+min_dist_bar_<-data.frame(min_dist_bar_)
+
+#Se incorpora la mínima distancia a bares al DTRAIN
+DTRAIN<-cbind(DTRAIN, min_dist_bar_ )
+DTRAIN_sf<-cbind(DTRAIN_sf, min_dist_bar_ )
 
 
-# bares
-## bares
-#TPMed = opq(bbox = st_bbox("mnz"  )) %>%
- # add_osm_feature(key = "amenity", value = "bus_station") %>%
-  #osmdata_sf() %>% .$osm_points %>% select(osm_id,name)
-#bar %>% head()
-#DTRAIN_sf%>% head
-## Pintar las transporte publico
+#Para Test:
+rm(min_dist_bar_)
+
+dist_bar_test_bog <- st_distance(x=DTEST_sf , y=barbog)
+min_dist_test_bog <- apply(dist_bar_test_bog , 1 , min)
+min_dist_test_bog<-data.frame(min_dist_test_bog)
+
+#Con respecto a bares Antioquia
+dist_bar_test_med <- st_distance(x=DTEST_sf , y=barant)
+min_dist_bar_test_med <- apply(dist_bar_test_med, 1 , min)
+min_dist_bar_test_med<-data.frame(min_dist_bar_test_med)
+
+#Se selecciona la menor distancia, esto porque se compararon distancias de apartamentos en Bogotá con ubicaciones en Antioquia y viceversa
+min_dist_test_bar<-cbind(min_dist_test_bog,min_dist_bar_test_med)
+min_dist_bar_<- apply(min_dist_test_bar, 1 , min)
+min_dist_bar_<-data.frame(min_dist_bar_)
+
+#Se incorpora la mínima distancia a bares al DTRAIN
+DTEST<-cbind(DTEST, min_dist_bar_ )
+DTEST_sf<-cbind(DTEST_sf, min_dist_bar_ )
+
+
+
+#Se obtendrá la distancia mínima a transporte público
+
+#Con respecto a transporte público en Bogotá
+dist_transp_bog<- st_distance(x=DTRAIN_sf , y=Transporte_publicoBog)
+min_dist_transp_bog <- apply(dist_transp_bog , 1 , min)
+min_dist_transp_bog<-data.frame(min_dist_transp_bog)
+
+#Con respecto a transporte público en Medellín
+dist_transp_med <- st_distance(x=DTRAIN_sf , y=Transporte_publicoMed)
+min_dist_transp_med <- apply(dist_transp_med, 1 , min)
+min_dist_transp_med<-data.frame(min_dist_transp_med)
+
+#Se selecciona la menor distancia, esto porque se compararon distancias de apartamentos en Bogotá con ubicaciones en Antioquia y viceversa
+min_dist_transp<-cbind(min_dist_transp_bog,min_dist_transp_med)
+min_dist_transp_<- apply(min_dist_transp, 1 , min)
+min_dist_transp_<-data.frame(min_dist_transp_)
+
+#Se incorpora la mínima distancia a bares al DTRAIN
+DTRAIN<-cbind(DTRAIN, min_dist_transp_)
+DTRAIN_sf<-cbind(DTRAIN_sf, min_dist_transp_ )
+
+#Para Test
+rm(min_dist_transp_)
+#Con respecto a transporte público en Bogotá
+dist_transp_test_bog<- st_distance(x=DTEST_sf , y=Transporte_publicoBog)
+min_dist_transp_test_bog <- apply(dist_transp_test_bog , 1 , min)
+min_dist_transp_test_bog<-data.frame(min_dist_transp_test_bog)
+
+#Con respecto a transporte público en Medellín
+dist_transp_test_med <- st_distance(x=DTEST_sf , y=Transporte_publicoMed)
+min_dist_transp_test_med <- apply(dist_transp_test_med, 1 , min)
+min_dist_transp_test_med<-data.frame(min_dist_transp_test_med)
+
+#Se selecciona la menor distancia, esto porque se compararon distancias de apartamentos en Bogotá con ubicaciones en Antioquia y viceversa
+min_dist_test_transp<-cbind(min_dist_transp_test_bog,min_dist_transp_test_med)
+min_dist_transp_<- apply(min_dist_test_transp, 1 , min)
+min_dist_transp_<-data.frame(min_dist_transp_)
+
+#Se incorpora la mínima distancia a bares al DTRAIN
+DTEST<-cbind(DTEST, min_dist_transp_)
+DTEST_sf<-cbind(DTEST_sf, min_dist_transp_)
+
 
 
 
