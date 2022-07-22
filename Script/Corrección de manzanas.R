d1c33a98aab83fd9ae7e15463a0ba4cef2d3dab7
mvgaona <mv.gaona@uniandes.edu.co>
2022-07-22 13:48:48 -0500

    Actualización

--- Script/Corrección de manzanas.R
+++ Script/Corrección de manzanas.R
@@ -62,14 +62,14 @@ house_chapinero_mnz <- st_join(House_Chapinero, mnz_chap)
 colnames(house_chapinero_mnz)
 table(is.na(house_chapinero_mnz$MANZ_CCNCT))
 db_1 <- house_chapinero_mnz %>% subset(is.na(MANZ_CCNCT)==F)
-db_2 <- house_chapinero_mnz %>% subset(is.na(MANZ_CCNCT)==T) %>% mutate(MANZ_CCNCT)
+db_2 <- house_chapinero_mnz %>% subset(is.na(MANZ_CCNCT)==T) %>% mutate(MANZ_CCNCT = NULL)
 leaflet() %>% addTiles() %>% addPolygons(data=db_2[1,] %>% st_buffer(dist = 0.0005))
 db_2 <- st_join(st_buffer(db_2, dist = 0.0005), mnz_chap)%>% subset(duplicated(property_id)==F)
 #MANNZ
-table(is.na(db_2$MANZ_CCNCT))
+table(is.na(db_2$MANZ_CCNCT.y))
 Casa_mnz_Chap = st_join(x = db_2,y = mnz_chap)
 Casa_mnz_Chap = Casa_mnz_Chap %>%
-  group_by(MANZ_CCNCT) %>%
+  group_by(MANZ_CCNCT.y) %>%
   mutate(new_surface_2=median(surface_total,na.rm=T))
 table(is.na(Casa_mnz_Chap$new_surface_2))
 table(is.na(Casa_mnz_Chap$surface_total),
@@ -81,14 +81,15 @@ st_geometry(Casa_buf) = NULL
 Casa_buf_mean = Casa_buf %>% group_by(property_id) %>% summarise(surface_new_3=mean(surface_total.y,na.rm=T))
 
 Casa_mnz_Chap = left_join(Casa_mnz_Chap,Casa_buf_mean,"property_id")
-Casa_mnz_aux<- st_join(HOUSE,mnz_chap,join = nngeo::st_nn, maxdist = 0.0005, k = 1, progress = FALSE)
 
-test = is.na(Casa_mnz_aux$surface_total)
-no_test = is.na(Casa_mnz_aux$surface_total)==F
+test = is.na(Casa_mnz_Chap$surface_total)
+no_test = is.na(Casa_mnz_Chap$surface_total)==F
 
+p_load(caret)
+require(caret)
 
-k1 = knn(train=Casa_mnz_aux[no_test,c("geometry","surface_total")], ## base de entrenamiento
-         test=Casa_mnz_aux[test,c("geometry","surface_total")],   ## base de testeo
-         cl=Casa_mnz_aux$surface_total[test], ## outcome
+k1 = knn(train=Casa_mnz_Chap[no_test,c("geometry","surface_total")], ## base de entrenamiento
+         test=Casa_mnz_Chap[test,c("geometry","surface_total")],   ## base de testeo
+         cl=Casa_mnz_Chap$surface_total[test], ## outcome
          k=1)  
 
